21ae48afb75e82633a00d5855d7ee171158b8b91
Zokero <piotrekkukiela@gmail.com>
2019-06-08 00:24:01 +0200

    First Commit

---
+++ src/main/java/com/example/HessianClientServer/HessianClientServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.HessianClientServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HessianClientServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HessianClientServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HessianClientServer/HessianClientServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.HessianClientServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HessianClientServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
